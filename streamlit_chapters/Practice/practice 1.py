9dcbc341f29e21a21d095d4dfad2aeb7caffe871
nishant050 <pb22mtech11001@iith.ac.in>
2025-07-22 19:34:27 +0530

    Go

--- streamlit_chapters/Practice/practice 1.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-st.title("My Indian Grand Feast Thali")
---
+++ streamlit_chapters/Practice/practice1.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("My Indian Grand Feast Thali")
+
+st.header("Welcome to my Indian Thali, Enjoy your food")
+
+st.write('Indian thalis are most tradational foods offering tasty foods')
+
+if st.button('Get a Thali'):
+    st.write("here is your Thali")
+    st.balloons()
+
+st.write('This is start of my app')
+st.write(43)
+st.write('It can handle both numbers and text')
